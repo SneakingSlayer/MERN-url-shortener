1a64abef546aaf5830ee5e4ee454aec9e4931c5b
SneakingSlayer <endayalance@yahoo.com>
2022-02-02 15:03:26 +0800

    bugs & fixes

--- client/src/globals/globals.js
+++ client/src/globals/globals.js
@@ -1,14 +1,13 @@
-const jwt = () =>{
-    if(localStorage.getItem('user')==="null" || !localStorage.getItem('user'))
-        return ''
-    if(localStorage.getItem('user')!=="null"){
-        const {token} = JSON.parse(localStorage.getItem('user'))
-        return token
-    }     
-}
-export const BASE_URL = ""
-export const config =  {
-    'Content-Type': 'application/json',
-    'Authorization': jwt()
-}
-
+const jwt = () => {
+  if (localStorage.getItem("user") === "null" || !localStorage.getItem("user"))
+    return "";
+  if (localStorage.getItem("user") !== "null") {
+    const { token } = JSON.parse(localStorage.getItem("user"));
+    return token;
+  }
+};
+export const BASE_URL = "";
+export const config = {
+  "Content-Type": "application/json",
+  Authorization: jwt(),
+};
